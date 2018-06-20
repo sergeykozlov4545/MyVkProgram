958c603f15e81ea2f67f8a7028923a1e2599c9cf
sergeykozlov4545 <sergeykozlov4545@gmail.com>
2018-06-20 22:44:55 +0500

    Добавление фрагментов для bottom bar

--- app/src/main/java/com/example/sergey/myvkprogram/MainActivity.java
+++ app/src/main/java/com/example/sergey/myvkprogram/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.sergey.myvkprogram;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.example.sergey.myvkprogram.fragments.AlbumsFragment;
+import com.example.sergey.myvkprogram.fragments.FriendsFragment;
+import com.example.sergey.myvkprogram.fragments.GroupsFragment;
+import com.example.sergey.myvkprogram.fragments.VideosFragment;
 import com.example.sergey.myvkprogram.listeners.MainActivityBottomBarTabSelectListener;
 import com.roughike.bottombar.BottomBar;
 
@@ -18,6 +25,70 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         BottomBar bottomBar = findViewById(R.id.bottomBar);
-        bottomBar.setOnTabSelectListener(new MainActivityBottomBarTabSelectListener());
+        bottomBar.setOnTabSelectListener(new MainActivityBottomBarTabSelectListener(this));
+
+        showAlbums();
+    }
+
+    public void onTabSelected(int tabId) {
+        Log.e("MyTag", "onTabSelected: tabId = " + tabId);
+
+        if (tabId == R.id.albums) {
+            showAlbums();
+            return;
+        }
+        if (tabId == R.id.videos) {
+            showVideos();
+            return;
+        }
+        if (tabId == R.id.friends) {
+            showFriends();
+            return;
+        }
+        if (tabId == R.id.groups) {
+            showGroups();
+            return;
+        }
+    }
+
+    private void showAlbums() {
+        showFragment(getFragmentByTabId(R.id.albums));
+    }
+
+    private void showVideos() {
+        showFragment(getFragmentByTabId(R.id.videos));
+    }
+
+    private void showFriends() {
+        showFragment(getFragmentByTabId(R.id.friends));
+    }
+
+    private void showGroups() {
+        showFragment(getFragmentByTabId(R.id.groups));
+    }
+
+    private Fragment getFragmentByTabId(int tabId) {
+        if (tabId == R.id.albums) {
+            return new AlbumsFragment();
+        }
+        if (tabId == R.id.videos) {
+            return new VideosFragment();
+        }
+        if (tabId == R.id.friends) {
+            return new FriendsFragment();
+        }
+        if (tabId == R.id.groups) {
+            return new GroupsFragment();
+        }
+        return null;
+    }
+
+    private void showFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.contentContainer, fragment)
+                .commit();
+
+        Log.e("MyTag", "showFragment: fragment = " + fragment);
     }
 }
--- app/src/main/java/com/example/sergey/myvkprogram/listeners/MainActivityBottomBarTabSelectListener.java
+++ app/src/main/java/com/example/sergey/myvkprogram/listeners/MainActivityBottomBarTabSelectListener.java
@@ -1,10 +1,18 @@
 package com.example.sergey.myvkprogram.listeners;
 
+import com.example.sergey.myvkprogram.MainActivity;
 import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivityBottomBarTabSelectListener implements OnTabSelectListener {
 
+    private MainActivity mainActivity;
+
+    public MainActivityBottomBarTabSelectListener(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+
     @Override
     public void onTabSelected(int tabId) {
+        mainActivity.onTabSelected(tabId);
     }
 }
