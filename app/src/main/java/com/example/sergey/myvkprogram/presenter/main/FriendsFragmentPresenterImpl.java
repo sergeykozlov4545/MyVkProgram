e3c6a1563430eaff8cb1bda96458243ca8095212
Kozlov Sergey <sergeykozlov4545@gmail.com>
2018-06-22 17:39:37 +0500

    перенос Callback'а в презентер

--- app/src/main/java/com/example/sergey/myvkprogram/presenter/main/FriendsFragmentPresenterImpl.java
+++ app/src/main/java/com/example/sergey/myvkprogram/presenter/main/FriendsFragmentPresenterImpl.java
@@ -3,13 +3,16 @@ package com.example.sergey.myvkprogram.presenter.main;
 import android.support.annotation.NonNull;
 
 import com.example.sergey.myvkprogram.contracts.FriendsFragmentContract;
+import com.example.sergey.myvkprogram.model.pojo.FriendsResponse;
 import com.example.sergey.myvkprogram.model.pojo.User;
-import com.example.sergey.myvkprogram.model.retrofit.QueryParams.FriendsQueryParams;
-import com.example.sergey.myvkprogram.model.retrofit.ServiceApi.Constants;
 import com.example.sergey.myvkprogram.model.retrofit.ServiceApi.FriendsQueryServiceManager;
 import com.example.sergey.myvkprogram.presenter.base.BasePresenter;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FriendsFragmentPresenterImpl
         extends BasePresenter<FriendsFragmentContract.FriendsFragmentView>
@@ -27,7 +30,7 @@ public class FriendsFragmentPresenterImpl
             getView().showProgress();
         }
 
-        serviceManager.loadData(this);
+        serviceManager.loadData(new ServiceManagerCallback());
     }
 
     @Override
@@ -47,6 +50,28 @@ public class FriendsFragmentPresenterImpl
         if (view != null) {
             view.hideProgress();
             view.showError(message);
+        }
+    }
+
+    private class ServiceManagerCallback implements Callback<FriendsResponse> {
+
+        @Override
+        public void onResponse(Call<FriendsResponse> call, Response<FriendsResponse> response) {
+            if (response.isSuccessful()) {
+                FriendsResponse friendsResponse = response.body();
+                List<User> friends = friendsResponse.getItems();
+
+                if (friends != null) {
+                    friendsLoaded(friends);
+                }
+            } else {
+                friendsErrorLoaded("Произошла ошибка при выполнении запроса: code = " + response.code());
+            }
+        }
+
+        @Override
+        public void onFailure(Call<FriendsResponse> call, Throwable t) {
+            friendsErrorLoaded("Произошла ошибка при выполнении запроса");
         }
     }
 }
