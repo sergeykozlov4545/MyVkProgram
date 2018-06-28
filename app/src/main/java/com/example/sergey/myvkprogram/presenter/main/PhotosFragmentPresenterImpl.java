65fecee560203fe411a296fd7f16c8c3762172d7
sergeykozlov4545 <sergeykozlov4545@gmail.com>
2018-06-28 21:21:54 +0500

    фиксф после обновления ветки

--- app/src/main/java/com/example/sergey/myvkprogram/presenter/main/PhotosFragmentPresenterImpl.java
+++ app/src/main/java/com/example/sergey/myvkprogram/presenter/main/PhotosFragmentPresenterImpl.java
@@ -3,6 +3,8 @@ package com.example.sergey.myvkprogram.presenter.main;
 import android.support.annotation.NonNull;
 
 import com.example.sergey.myvkprogram.contracts.PhotosFragmentContract;
+import com.example.sergey.myvkprogram.model.managers.CacheManager.CacheObjects.CacheKey;
+import com.example.sergey.myvkprogram.model.managers.CacheManager.LocalCacheManager;
 import com.example.sergey.myvkprogram.model.managers.DataManager.CallbackLoadData;
 import com.example.sergey.myvkprogram.model.managers.DataManager.DataManager;
 import com.example.sergey.myvkprogram.model.pojo.object.Photo;
@@ -22,21 +24,7 @@ public class PhotosFragmentPresenterImpl
 
     @Override
     public void viewIsReady() {
-        if (getView() != null) {
-            getView().showProgress();
-        }
-
-        dataManager.getData(new CallbackLoadData<Photo>() {
-            @Override
-            public void onSuccessful(@NonNull List<Photo> data) {
-                photosLoaded(data);
-            }
-
-            @Override
-            public void onFailure(@NonNull String message) {
-                photosErrorLoaded(message);
-            }
-        });
+        loadData();
     }
 
     @Override
@@ -57,5 +45,31 @@ public class PhotosFragmentPresenterImpl
             view.hideProgress();
             view.showError(message);
         }
+    }
+
+    private void loadData() {
+        dataManager.getData(new CallbackLoadData<Photo>() {
+            @Override
+            public void onStartLoadData() {
+                if (getView() != null) {
+                    getView().showProgress();
+                }
+            }
+
+            @Override
+            public void onSuccessful(@NonNull List<Photo> data) {
+                LocalCacheManager.getInstance()
+                        .put(CacheKey.PhotosFragment.FIRST_VISIBLE, Boolean.FALSE);
+                LocalCacheManager.getInstance()
+                        .put(CacheKey.PhotosFragment.ITEMS_DATA, data);
+
+                photosLoaded(data);
+            }
+
+            @Override
+            public void onFailure(@NonNull String message) {
+                photosErrorLoaded(message);
+            }
+        });
     }
 }
